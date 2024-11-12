20a367b25e5ec5f0651b8100684dbbe45b81bbde
Chaitanya Pandey <chaitanya.pandey.python@gmail.com>
2024-11-12 17:08:51 +0530

    Initialize task scheduler backend project with Maven and Spring Boot

---
+++ src/main/java/com/scheduler/task_scheduler_backend/TaskSchedulerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.scheduler.task_scheduler_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskSchedulerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskSchedulerBackendApplication.class, args);
+	}
+
+}
